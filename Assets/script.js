c8f72d4fba50b52b49a7da9b8046048e39acdf0d
DanielkKostich <117228151+DanielkKostich@users.noreply.github.com>
2023-04-02 16:03:59 -0500

    added styling

--- Assets/script.js
+++ Assets/script.js
@@ -1,2 +1,23 @@
-key='git@github.com:DanielkKostich/weather-forcast.git'
-api='api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid='
+ var key='96229522a112929bafe99d9c89d7d638'
+var api='api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid='
+var city = document.querySelector('#dataList').value
+var string = api + key + city
+
+
+var firstcall ='http://api.openweathermap.org/geo/1.0/direct?q=belleville,us&limit=3&appid=' + key
+
+
+document.getElementById('dataList').value()
+
+var lat = ''
+var lon =''
+
+fetch(firstcall)
+   .then(function (response) {
+    return response.json();
+ })
+
+.then(function (data)  {
+    console.log(data);
+    document.getElementById('apiResponse').innerHTML = JSON.stringify(data);
+  });
