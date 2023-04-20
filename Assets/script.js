0d2816710b03d8d65c8739d348a1d1f2320fe329
DanielkKostich <117228151+DanielkKostich@users.noreply.github.com>
2023-04-20 12:46:43 -0500

    fixed logic

--- Assets/script.js
+++ Assets/script.js
@@ -1,23 +1,29 @@
- var key='96229522a112929bafe99d9c89d7d638'
-var api='api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid='
-var city = document.querySelector('#dataList').value
-var string = api + key + city
+var apiKey = '96229522a112929bafe99d9c89d7d638'
 
+async function getForcast(){
+   
+  
+  var city = document.querySelector('#dataList').value
+  //var weather = api + apiKey + city;
+  var firstcall = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`
+   await fetch(firstcall)
+  .then((response) => response.json())
+  .then((data) => {
+      if (data.length > 0 && data[0].lat && data[0].lon) {
+          const lat = data[0].lat;
+          const lon = data[0].lon;
+          // use lat and lon in the next fetch call to get the weather forecast
+          var api = "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid=" + apiKey
+          fetch(api)
+          .then((response) => response.json())
+          .then((forecast) => {
+            console.log(forecast);
+            // process the forecast data and update the UI
+          })
+          .catch((error) => console.log('Error: Could not get weather forecast from API'));
+      } else {
+          console.log('Error: Could not get latitude and longitude from response');
+      }
+  })
+  .catch((error) => console.log('Error: Could not get location from API'));}
 
-var firstcall ='http://api.openweathermap.org/geo/1.0/direct?q=belleville,us&limit=3&appid=' + key
-
-
-document.getElementById('dataList').value()
-
-var lat = ''
-var lon =''
-
-fetch(firstcall)
-   .then(function (response) {
-    return response.json();
- })
-
-.then(function (data)  {
-    console.log(data);
-    document.getElementById('apiResponse').innerHTML = JSON.stringify(data);
-  });
